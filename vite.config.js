65c043fe858a5952ca238b386ade8b8615f1d784
JanHunar <jan.hunar.jha@gmail.com>
2023-07-21 17:48:21 +0200

    config edited

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    build: {
+        output: 'app/public/build',
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/css/admin.css', 'resources/js/app.js'],
